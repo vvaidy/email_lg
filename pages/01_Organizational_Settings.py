458f4e4efa8c83f8c1dd949143156021ca7d7970
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-07-08 11:25:05 -0500

    Update 01_Organizational_Settings.py
    refactored

--- pages/01_Organizational_Settings.py
+++ pages/01_Organizational_Settings.py
@@ -9,12 +9,12 @@ if 'organizational_settings' not in st.session_state:
 
 org_settings = st.text_area("Organizational Settings", st.session_state.organizational_settings, height=420)
 st.session_state.organizational_settings = org_settings
-col1, col2, col3 = st.columns([.2,.3,.5])
-with col1:
+button_update, button_persist, saved_message = st.columns([.2,.3,.5])
+with button_update:
     if st.button("Update"):
         statechange(INITIAL_STATE,"organizational_settings", st.session_state.organizational_settings, False)
-with col2:
+with button_persist:
     if st.button("Persist"):
         statechange(INITIAL_STATE,"organizational_settings", st.session_state.organizational_settings, True)
-        with col3:
+        with saved_message:
             st.write("Saved...")
