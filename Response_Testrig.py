e47dbf231ba5d771a501704b2d7b7a578f0e0861
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-07-08 11:20:39 -0500

    Update Response_Testrig.py
    refactored to make it easier to read.

--- Response_Testrig.py
+++ Response_Testrig.py
@@ -15,23 +15,23 @@ if 'customer_email' not in st.session_state:
 st.set_page_config(layout="wide")
 
 st.title("Customer Service Workflow Example")
-left_column, right_column = st.columns(2)
+input_column, output_column = st.columns(2)
 
-with left_column:
+with input_column:
     cust_email = st.text_area(
         "Customer Email", st.session_state.customer_email,
         height=320)
     st.session_state.customer_email = cust_email
-    col1, col2, col3 = st.columns(3)
-    with col1:
+    respond_button, save_button, saved_message = st.columns(3)
+    with respond_button:
         if st.button("Respond"):
             with st.spinner("Generating Response..."):
                 statechange(INITIAL_STATE,"customer_email", st.session_state.customer_email, False)
                 response = run_responder()
-            with right_column:
+            with output_column:
                 st.info(response)
-    with col2:
+    with save_button:
         if st.button("Save"):
             statechange(INITIAL_STATE,"customer_email", st.session_state.customer_email, True)
-            with col3:
+            with saved_message:
                 st.write("Saved...")
