6847963ec7800cb67df4a36f266c0dfb043bdea6
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-07-08 11:27:12 -0500

    Update 02_Agent_Instructions.py
    refactored, removed remnants from the old system of reading categories from two separate text files to now read from INITIAL_STATE

--- pages/02_Agent_Instructions.py
+++ pages/02_Agent_Instructions.py
@@ -4,7 +4,7 @@ from helper import *
 
 st.markdown("# Instructions for Agents")
 
-left_column, right_column, blank_column = st.columns(3)
+options, numbers, spacer = st.columns(3)
 
 # Select box for agent creation
 # Currently Categorizer is the only one that works
@@ -56,7 +56,7 @@ if "categorizer_num" not in st.session_state:
 if "agent_selectbox_index" not in st.session_state:
     st.session_state.agent_selectbox_index = 0
 
-with left_column:
+with options:
     # Create Base Category selectbox, and save it to session_state
     base_option = st.selectbox(label="Base Category", options=category_selectbox_options, index = st.session_state.agent_selectbox_index)
     st.session_state.agent_selectbox_index = category_selectbox_options.index(base_option)
@@ -65,38 +65,38 @@ with left_column:
 if(base_option == "Categorizer"):
     state_len = len(categorizer_description)
     # Number of categories needed
-    with right_column:
+    with numbers:
         num_descriptions = st.number_input(label="Descriptions Needed", min_value = 1, max_value = max_categories, value=st.session_state.categorizer_num)
         st.session_state.categorizer_num = num_descriptions
-    left, mid, right = st.columns([.2,.4,.4])
+    category_name, description, instruction = st.columns([.2,.4,.4])
     st.write("""Built-in categories:\n
              'possible_adversarial_attack' - used if the response may be a malicious attack may try manipulating the use of AI
              'off_topic' - when it does not relate to any of the other categories
              """)
     # Titles for the two parts of the categories
-    with left:
+    with category_name:
         st.write("Category Name")
-    with mid:
+    with description:
         st.write("Description")
-    with right:
+    with instruction:
         st.write("Instructions")
     # Creates as many Categories and Descriptions as dictated from the Descriptions Needed number input
     for i in range(num_descriptions):
-        with left:
+        with category_name:
             label = "Category" + str(i)
             keys_value = ""
             if i < state_len:
                 keys_value = st.session_state.agent_descriptions_keys[i]
             keys_holder = st.text_area(label=label, label_visibility="hidden", height = 15, value = st.session_state.agent_descriptions_keys[i])
             st.session_state.agent_descriptions_keys[i] = keys_holder
-        with mid:
+        with description:
             label = "Description" + str(i)
             values_value = ""
             if i < state_len:
                 values_value = st.session_state.agent_descriptions_values[i]
             values_holder = st.text_area(label = label, label_visibility="hidden", height=15, value = st.session_state.agent_descriptions_values[i])
             st.session_state.agent_descriptions_values[i] = values_holder
-        with right:
+        with instruction:
             label = "Instruction" + str(i)
             values_value = ""
             if i < state_len:
@@ -104,19 +104,12 @@ if(base_option == "Categorizer"):
             values_holder = st.text_area(label = label, label_visibility="hidden", height=15, value = st.session_state.agent_instructions_values[i])
             st.session_state.agent_instructions_values[i] = values_holder
     # Save Button
-    with left:
+    with category_name:
         if st.button("Save"):
             result_description = dict(zip(st.session_state.agent_descriptions_keys, st.session_state.agent_descriptions_values))
             result_instruction = dict(zip(st.session_state.agent_descriptions_keys, st.session_state.agent_instructions_values))
             # Removes and empty spaces that can be created from the lists
-            if '' in result_description:
-                del result_description['']
-            if '' in result_instruction:
-                del result_instruction['']
-            print(result_description)
-            print(result_instruction)
-            # Currently needed, until CSAGENTS gets updated. A restart of the program is needed for the saved info to be used
-            json.dump(result_description, open("categories_description.txt", 'w'))
-            json.dump(result_instruction, open("categories_instruction.txt", 'w'))
+            remove_space(result_description)
+            remove_space(result_instruction)
             statechange(INITIAL_STATE,"agent_descriptions",result_description,True)
             statechange(INITIAL_STATE,"agent_instructions",result_instruction,True)
