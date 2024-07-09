7478d746582826cfdd0e998639b050020d00fade
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-07-09 15:24:35 -0500

    Update 04_Model.py
    Made the select box show what the currently selected choice for Model is.

--- pages/04_Model.py
+++ pages/04_Model.py
@@ -8,7 +8,12 @@ input_column, space_column= st.columns(2)
 
 model_options = ["llama3-8b-8192", "gemma2-9b-it", "mixtral-8x7b-32768"]
 
+if "model_index" not in st.session_state:
+    st.session_state.model_index = model_options.index(INITIAL_STATE.get("model"))
+
 with input_column:
-    model = st.selectbox("Model", model_options, 0)
+    model = st.selectbox("Model", model_options, st.session_state.model_index)
+    st.session_state.model_index = model_options.index(model)
     if st.button("Persist"):
         statechange(INITIAL_STATE, "model", model, True)
+st.text("A 'Persist' and a restart of the program is needed to change models")
